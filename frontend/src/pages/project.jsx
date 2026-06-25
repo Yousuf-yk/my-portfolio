21abca81909a4bd46dc78fbc3519fcba6c45a10e
yousuf-yk <yk0574292@gmail.com>
2026-06-25 13:55:06 +0530

    updated project section and mediaQuery

--- frontend/src/pages/project.jsx
+++ frontend/src/pages/project.jsx
@@ -1,17 +1,21 @@
 import "../index.css";
+import weatherApp from "../assets/photos/weatherApp.png";
+import Ecommerce from "../assets/photos/basicEcommerce.png";
 
 const projects = [
   {
     id: 1,
     name: "basic-ecommerce",
-    image: "/images/portfolio.png",
-    description: "Personal portfolio built using React."
+    image: Ecommerce,
+    description: "prototype of ecommerce using ejs, node, express, postrgres.",
+    github: "https://github.com/Yousuf-yk/basic_ecommerce.git"
   },
   {
     id: 2,
-    name: "e-commerce",
-    image: "/images/weather.png",
-    description: "Displays real-time weather information."
+    name: "weather-app",
+    image:  weatherApp,
+    description: "Displays real-time weather information.",
+    github: "https://github.com/Yousuf-yk/weather-app.git"
   },
 ]
 function Projects() {
@@ -20,15 +24,20 @@ function Projects() {
             <h2 className="project-title">project</h2>
             <div className="projects-grid">
                 {projects.map((project) => (
+                    
                     <div className="projects-card" key={project.id} >
                         <img 
-                        src={project.src}
-                        alt={project.alt}
+                        src={project.image}
+                        alt={project.name}
                         className="project-img"
                         />
                         <h3>{project.name}</h3>
                         <p>{project.description}</p>
+                        <a href={project.github} target="_blank" rel="noopener noreferrer" className="project-btn">
+              View Code
+            </a>
                     </div>
+
                 ))}
             </div>
         </section>
