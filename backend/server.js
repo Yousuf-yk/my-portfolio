5606eabe0db468b424aa5c4fd87d8997891a3e07
yousuf-yk <yk0574292@gmail.com>
2026-08-04 18:33:28 +0530

    path for cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://yousufKhan.vercel.app"
+}));
 app.use(express.json());
 
 app.post("/contact", async (req, res) => {
